151f6350f1f6d4e04b52956e782e2192b553ffe8
Wallacy V. <wlcsv8080@outlook.com>
2025-01-01 15:31:59 -0300

    Quero terminar logo isso para refazer o tutorial de flashcards

--- projects/models.py
+++ projects/models.py
@@ -4,3 +4,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
